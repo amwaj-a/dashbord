8c3e344006ab897653245a77360e22399417d70f
Amwaj <amwaj.1431@outlook.com>
2024-07-09 10:15:57 +0300

    v2

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import HomeAdmin from './HomeAdmin'
 import HomeUser from './HomeUser'
+import Nav from './component/Nav'
 
 export default function Home() {
     let email=localStorage.getItem('email')
   return (
     <>
+    <Nav/>
     {/* {console.log(!localStorage.getItem('email').includes('domain'))} */}
     {email.includes('admin')?<HomeAdmin/>:<HomeUser/>}
-    
+  
     
     </>
    
